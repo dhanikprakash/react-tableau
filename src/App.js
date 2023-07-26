fc4c1285ff4754e28bf33478fd0e7b60b0bd875b
dhanikprakash <dhanikprakash@gmail.com>
2023-07-26 12:22:13 +0100

    update not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BasicEmbed from "./components/BasicEmbed";
+import Notfound from "./components/Notfound";
 
 function App() {
   return (
     <div className="App">
       <h1>Tableau Embed</h1>
+      {/* <Notfound /> */}
       <BasicEmbed />
     </div>
   );
